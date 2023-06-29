0e2bde1ba5c2a55f7a8bf03ccfcc9e2dbae592fb
zhenghong <chris3352754895@gmail.com>
2023-06-29 03:24:08 -0500

    full app

--- app/streaming/_components/header.tsx
+++ app/streaming/_components/header.tsx
@@ -8,11 +8,6 @@ export function Header() {
   return (
     <div className="flex items-center justify-between gap-x-3 rounded-lg bg-gray-800 px-3 py-3 lg:px-5 lg:py-4">
       <div className="flex gap-x-3">
-        <Link href="/streaming">
-          <div className="h-10 w-10 hover:opacity-70">
-            <NextLogo />
-          </div>
-        </Link>
 
         <div className="relative flex-1">
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
@@ -29,22 +24,7 @@ export function Header() {
         </div>
       </div>
 
-      <div className="flex shrink-0 gap-x-3">
-        <div className="relative flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-gray-600 text-white">
-          <ShoppingCartIcon className="w-6 text-white" />
-          <div className="absolute -top-1 -right-1 flex h-4 w-4 items-center justify-center rounded-full bg-vercel-cyan text-sm font-bold text-cyan-800">
-            <CartCount />
-          </div>
-        </div>
 
-        <Image
-          src="/prince-akachi-LWkFHEGpleE-unsplash.jpg"
-          className="rounded-full"
-          width={40}
-          height={40}
-          alt="User"
-        />
-      </div>
     </div>
   );
 }
--- app/streaming/edge/layout.tsx
+++ app/streaming/edge/layout.tsx
@@ -13,29 +13,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <div className="prose prose-sm prose-invert mb-8 max-w-none">
-        <ul>
-          <li>
-            Primary product information is loaded first as part of the initial
-            response.
-          </li>
-          <li>
-            Secondary, more personalized details (that might be slower) like
-            ship date, other recommended products, and customer reviews are
-            progressively streamed in.
-          </li>
-          <li>Try refreshing or navigating to other recommended products.</li>
-        </ul>
-      </div>
+
 
       <Boundary animateRerendering={false} labels={['Demo']} size="small">
-        <CartCountProvider initialCartCount={cartCount}>
+
           <div className="space-y-10">
+            <li>test</li>
             <Header />
 
             {children}
           </div>
-        </CartCountProvider>
+
       </Boundary>
     </>
   );
